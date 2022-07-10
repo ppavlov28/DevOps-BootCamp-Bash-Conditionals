d594218c6ff77d98b31cee796187f3f2cd8a8c8d
ppavlov28 <ppavlov28@gmail.com>
2022-07-10 16:09:12 +0300

    Update temp.sh
    v1

--- temp.sh
+++ temp.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+input_temp=$1
+
+k_temp="K"
+
+c_temp="C"
+
+case $input_temp in
+    *$k_temp)
+        let "output_temp = ${input_temp%$k_temp} - 273"
+        echo $output_temp"C"
+        ;;
+    
+    *$c_temp)
+        let "output_temp = ${input_temp%$c_temp} + 273"
+        echo $output_temp"K"
+        ;;
+esac
+
 # Place your code here
