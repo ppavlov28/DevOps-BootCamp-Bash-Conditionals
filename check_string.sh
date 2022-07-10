1c8010b18dfee4f85c69c1ebeb4bd73c6fc2edd7
ppavlov28 <ppavlov28@gmail.com>
2022-07-10 16:07:56 +0300

    Update check_string.sh
    v1

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+num_list=$1
+
+list_sep=","
+
+arr_sep=" "
+
+num_list=(${num_list//$list_sep/$arr_sep})
+
+for item in ${num_list[@]}
+do
+	if [[ $(( $item % 2 )) = 0 ]]
+    then
+        num_sum=$(( $num_sum + $item ))
+    fi
+done
+
+echo $num_sum
+
 # Place your code here
