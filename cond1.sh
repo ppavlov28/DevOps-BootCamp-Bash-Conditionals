81a40b58416e551efa8aaf0bcd7056688ce16e38
ppavlov28 <ppavlov28@gmail.com>
2022-07-10 16:17:50 +0300

    Update cond1.sh
    v1

--- cond1.sh
+++ cond1.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-IFS=',' read -r -a array <<< "$1"
+#IFS=',' read -r -a array <<< "$1"
+
+num_list=$1
+
+list_sep=","
+
+arr_sep=" "
+
+num_list=(${num_list//$list_sep/$arr_sep})
+
+for item in ${num_list[@]}
+do
+	if [[ $(( $item % 2 )) = 0 ]]
+    then
+        num_sum=$(( $num_sum + $item ))
+    fi
+done
+
+echo $num_sum
 
 #Write your code here
